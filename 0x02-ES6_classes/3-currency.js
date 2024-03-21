530dc5ecce70b298cf32365b53a1226a58ab6015
Tamara Lumumba <tlumumba@gmail.com>
2024-03-21 11:45:40 +0000

    Adding updateed files

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,39 +1,38 @@
-class Currency {
+export default class Currency {
   constructor(code, name) {
-    if (typeof code !== 'string' || typeof name !== 'string') {
-      throw new Error('Inavlid input type');
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    } else if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
+
     this._code = code;
     this._name = name;
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  set code(value) {
-    if (typeof value !== 'string') {
-      throw new Error('invalid input');
-    } else {
-      this._code = value;
-    }
   }
 
   get name() {
     return this._name;
   }
 
-  set name(value) {
-    if (typeof value !== 'string') {
-      throw new Error('invalid input');
-    } else {
-      this._name = value;
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
+    this._name = name;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = code;
   }
 
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
 }
-
-export default Currency;
